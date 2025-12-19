30beda85934b5d8a8351f18c78fc9ba7238a433c
CHANDRA LAKSHMAN MAHESH POTNURI <95182557+chandramah@users.noreply.github.com>
2025-12-19 17:50:17 +0530

    Update API_BASE_URL to new cloudfront endpoint

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export const API_BASE_URL = "https://d3qs09ype2r7pk.cloudfront.net" || "http://localhost:8080";
+export const API_BASE_URL = "https://d1ujpx8cjlbvx.cloudfront.net" || "http://localhost:8080";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
