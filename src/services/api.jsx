fcff20d555aaa5dd995125696fc06eab051bd02e
RAYNX-SYTEMS-PRIVATE-LIMITED <raynxsystemspvtltd@gmail.com>
2025-12-24 15:02:55 +0530

    updated url cloud front

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export const API_BASE_URL = "http://localhost:8080"
+export const API_BASE_URL = "https://d1ujpx8cjlbvx.cloudfront.net"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
